a7cd890dc75d074be38df19cc449a4cfc45495dd
Vinit <vinitkumardevani@gmail.com>
2022-10-12 14:50:02 +0530

    add toggle and image link

--- src/Components/QuizApp/QuizApp.js
+++ src/Components/QuizApp/QuizApp.js
@@ -2,9 +2,42 @@ import React from 'react';
 import './QuizApp.css';
 import data from "./QuizAppData.json"
 import {useNavigate} from 'react-router-dom'
+import styled from "@emotion/styled";
+import Badge from '@mui/material/Badge';
+
 
 const QuizApp = () => {
-    const navigate = useNavigate();
+
+    const StyledBadge = styled(Badge)(() => ({
+        '& .MuiBadge-badge': {
+            backgroundColor: '#55e6c1',
+            color: '#55e6c1',
+            width: "12px",
+            height: "12px",
+            borderRadius: "10px",
+            '&::after': {
+                position: 'relative',
+                top: 0,
+                left: 0,
+                width: '11px',
+                height: '6px',
+                borderRadius: '10px',
+                animation: 'ripple 1s infinite ease-in-out',
+                border: '3px solid currentColor',
+                content: '""',
+            },
+        },
+        '@keyframes ripple': {
+            '0%': {
+                transform: 'scale(.8)',
+                opacity: 1,
+            },
+            '100%': {
+                transform: 'scale(2)',
+                opacity: 0,
+            },
+        },
+    }));
     return (
         <div className="main">
             <div className="header">
@@ -31,12 +64,14 @@ const QuizApp = () => {
                             <div className="quiz-card">
                                 <div className="card-content">
                                     <div className="thumbnial-img">
-                                        <img src="https://play240.atmequiz.com/img/films-and-celebraty.png" alt="FILM AND CELEBRITY Contest Quiz"/>
+                                        <img src={item?.cardImg} alt="FILM AND CELEBRITY Contest Quiz"/>
                                     </div>
                                     <div className="quiz-name-status">
                                         <div className="category-live-div">
                                             <span className="categories">{item.heading}</span>
-                                            {item.isLive && <span className="status-live">Live</span>}
+                                            {item.isLive && <span className="status-live">
+                                                <StyledBadge overlap="circular" anchorOrigin={{vertical: 'left', horizontal: 'right'}}
+                                                             variant="dot"> </StyledBadge>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Live</span>}
                                         </div>
                                         <h3>Play and Win {item.WinAmount} &nbsp;<img src="https://play240.atmequiz.com/img/coin-icon.png" width="19"
                                                                                      alt="coin"/></h3>
